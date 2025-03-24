6e1a33c67cb376760f7bfde979b57e055fbe9221
Kunlun-Donkey <1298394344@qq.com>
2025-03-24 13:50:46 +0800

    add oled_faces files

---
+++ USER/oled_faces.h
@@ -0,0 +1,15 @@
+#ifndef __OLED_FACES_H
+#define __OLED_FACES_H
+
+#include "stm32f10x.h"
+
+// 表情数据
+extern const uint8_t Face_hello[];
+extern const uint8_t Face_eyes[];
+extern const uint8_t Face_happy[];
+extern const uint8_t Face_mania[];
+extern const uint8_t Face_sleep[];
+extern const uint8_t Face_stare[];
+extern const uint8_t Face_very_happy[];
+
+#endif // __OLED_FACES_H
